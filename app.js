cb0c45db0d79636fa692838c8881953ba94df8ff
Elena Vereshchagina <Elen.Vereshchagina@gmail.com>
2023-08-05 01:48:09 +0300

    Add schemas and models

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 const app = express();
 
