718244fd48a0e6fc09fd3a04b480b6129c324140
Elena Vereshchagina <Elen.Vereshchagina@gmail.com>
2023-08-20 02:43:55 +0300

    bugfix after tests 4

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -17,10 +17,8 @@ const validateGetUserById = celebrate({
 
 const validatePostUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required()
-      .default('Жак-Ив Кусто'),
-    about: Joi.string().min(2).max(30).required()
-      .default('Исследователь'),
+    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
+    about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string().pattern(regularUrl).default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
@@ -29,20 +27,20 @@ const validatePostUser = celebrate({
 
 const validatePatchUserInfo = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 });
 
 const validatePatchAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(regularUrl),
+    avatar: Joi.string().pattern(regularUrl),
   }),
 });
 
 const validateGetCardById = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
+    cardId: Joi.string().length(24).hex(),
   }),
 });
 
