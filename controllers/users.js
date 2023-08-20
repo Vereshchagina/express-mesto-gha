dc01866283bd3e1c7371b96297dd271162dd9199
Elena Vereshchagina <Elen.Vereshchagina@gmail.com>
2023-08-20 03:17:13 +0300

    bugfix after tests 5

--- controllers/users.js
+++ controllers/users.js
@@ -114,11 +114,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-      })
-        .send({ token });
+      res.send({ token });
     })
     .catch(() => {
       next(new UnauthorizedError('Неправильные почта или пароль'));
